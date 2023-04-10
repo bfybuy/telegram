0d861b918d292363c1230e245f09b108691c9de1
Samuel Olaegbe <olaegbesamuel@gmail.com>
2023-04-10 06:50:44 +0100

    Add a bfybuy certificate and add port to .env

--- app.js
+++ app.js
@@ -5,46 +5,43 @@ import path from 'path';
 
 dotenv.config()
 
-// Function executed through Lambda
-export function Telegram()
-{
-  const token = process.env.TELEGRAM_TOKEN
+const token = process.env.TELEGRAM_TOKEN
 
-  const options = {
-    webHook: {
-      key: path.resolve('./localhost.key'),
-      cert: path.resolve('./localhost.crt')
-    }
+const options = {
+  webHook: {
+    port: process.env.APP_PORT,
+    key: './bfybuy.key',
+    cert: './bfybuy.crt'
   }
-
-  // Create a bot that uses 'polling' to fetch new updates
-  const bot = new TelegramBot(token, options);
-
-  // Setup webhook
-  bot.setWebHook(`${process.env.APP_URL}/bot${token}`, {
-    certificate: options.webHook.cert
-  })
-
-
-  // Matches "/echo [whatever]"
-  bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-  });
-
-  // Listen for any kind of message. There are different kinds of
-  // messages.
-  bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message with chat id ' + chatId);
-  });
 }
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, options);
+
+// Setup webhook
+bot.setWebHook(`${process.env.APP_URL}/webhook/${token}`, {
+  certificate: options.webHook.cert
+})
+
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message wiith chat id ' + chatId);
+});
